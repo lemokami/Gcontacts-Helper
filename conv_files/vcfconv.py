8095479fb52491f3f9688918676c2b8a7cfe3bd7
jesswinw <jesswinw@gmail.com>
2019-12-05 18:46:55 +0530

    convertion running OK but 3 word names have issues

--- conv_files/vcfconv.py
+++ conv_files/vcfconv.py
@@ -1,5 +1,4 @@
-import base64 as b64
-
+from conv_files import icomb
 def conv(param_dict):
     with open("decoded-vcf.vcf","w+") as f:
         for i in param_dict.keys():
@@ -10,8 +9,11 @@ def conv(param_dict):
             f.write(f"N:{ln};{i.split()[0]};;;\n")
             f.write(f"FN:{i}\n")
             f.write(f"TEL;CELL: {param_dict[i]['Phone 1 - Value']}\n")
-            f.write("PHOTO;ENCODING=BASE64;JPEG:{}\n")
 
+            try:
+                f.write(f"PHOTO;ENCODING=BASE64;JPEG:{icomb.imgmaker(param_dict[i]['Photo'])}\n")
+                print(f"downloading {i}'s image")
+            except:pass
             try:
                 f.write(f"NOTE:Birthday : {param_dict[i]['Birthday']}\n")
                 f.write("END:VCARD\n")
