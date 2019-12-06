41ed58ba55ad83aec04aa41ef13af7cb988ea3ca
jesswinw <jesswinw@gmail.com>
2019-12-06 18:18:57 +0530

    Fixed the issue

--- conv_files/vcfconv.py
+++ conv_files/vcfconv.py
@@ -5,7 +5,7 @@ def conv(param_dict):
             ln=''
             f.write("BEGIN:VCARD\nVERSION:2.1\n")
             if (i.split()[-1]) != (i.split()[0]):
-                ln = i.split()[-1]
+                ln =" ".join(i.split()[1:])
             f.write(f"N:{ln};{i.split()[0]};;;\n")
             f.write(f"FN:{i}\n")
             f.write(f"TEL;CELL: {param_dict[i]['Phone 1 - Value']}\n")
