17dded3d7dd47c6b19d3c13e2f78f8f9d85f4cd2
jesswinw <jesswinw@gmail.com>
2019-12-07 10:55:40 +0530

    minor changes to help the user

--- conv_files/vcfconv.py
+++ conv_files/vcfconv.py
@@ -1,5 +1,11 @@
 from conv_files import icomb
 def conv(param_dict):
+    while(1):
+        choice = input("[VCF]:Do you want to download images?[Y/N]:").upper()
+        if(choice=='Y' or choice=='N'):
+            break
+        else:
+            print("Wrong Input")
     with open("decoded-vcf.vcf","w+") as f:
         for i in param_dict.keys():
             ln=''
@@ -10,10 +16,12 @@ def conv(param_dict):
             f.write(f"FN:{i}\n")
             f.write(f"TEL;CELL: {param_dict[i]['Phone 1 - Value']}\n")
 
-            try:
-                f.write(f"PHOTO;ENCODING=BASE64;JPEG:{icomb.imgmaker(param_dict[i]['Photo'])}\n")
-                print(f"downloading {i}'s image")
-            except:pass
+            if(choice=='Y'):
+                try:
+                    f.write(f"PHOTO;ENCODING=BASE64;JPEG:{icomb.imgmaker(param_dict[i]['Photo'])}\n")
+                    print(f"downloading {i}'s image")
+                except:pass
+                
             try:
                 f.write(f"NOTE:Birthday : {param_dict[i]['Birthday']}\n")
                 f.write("END:VCARD\n")
